08ebaa94ddaf3b40d4058b8e36a9d9c100e2a5e7
speechgenius <shaabandaudi@gmail.com>
2024-08-19 01:18:03 +0300

    edited next.config images.domains to images.remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['t3.ftcdn.net'], // Add your external domains here
-      }, 
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 't3.ftcdn.net',
+            pathname: '/**', // Match any path
+          },
+          {
+                  protocol: 'https',
+                  hostname: 'mtonnsxvkvxzdhnzfxwb.supabase.co',
+                  port: '',
+                  pathname: '/storage/v1/object/public/files/**',
+                },
+        ],
+      },
 };
-
-
-// const nextConfig = { images: {
-//   remotePatterns: [
-//     {
-//       protocol: 'https',
-//       hostname: 'mtonnsxvkvxzdhnzfxwb.supabase.co',
-//       port: '',
-//       pathname: '/storage/v1/object/public/files/**',
-//     },
-//   ],
-// },}
 
 export default nextConfig;
