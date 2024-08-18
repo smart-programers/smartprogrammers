966eee8e0b7839700154048bb5b3ec8da97a87dc
Revaycolizer <albinuslucas@gmail.com>
2024-08-18 18:18:11 +0300

    Added TopLoader Changes to be committed: 	modified:   app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster"
-
+import NextTopLoader from 'nextjs-toploader';
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Toaster/>
+        <NextTopLoader />
         {children}</body>
     </html>
   );
