8d121373c0788e845925515c58452d7baa13a387
Henryle-hd <henrydionizi@gmail.com>
2024-08-19 12:35:34 +0300

    course image!

--- components/CourseCard.tsx
+++ components/CourseCard.tsx
@@ -1,22 +1,43 @@
-import React from 'react';
-import Services from './Services';
+import React from "react";
+import Services from "./Services";
+import Image from "next/image";
 
 interface CourseCardProps {
   title: string;
   price: string;
   students: number;
   description: string;
+  image: String;
 }
 
-const CourseCard: React.FC<CourseCardProps> = ({ title, price, students, description }) => {
+const CourseCard: React.FC<CourseCardProps> = ({
+  title,
+  price,
+  students,
+  description,
+  image,
+}) => {
   return (
     <div className="p-4 rounded-lg shadow-lg transition-shadow duration-300 dark:bg-gray-800 bg-white text-center hover:shadow-xl">
+      <div className="mb-4">
+        <Image
+          src={image as string}
+          width={400}
+          height={400}
+          alt={title}
+          className="w-full h-auto object-cover rounded-t-lg"
+        />
+      </div>
       <h3 className="text-xl font-bold mb-2 dark:text-white">{title}</h3>
       <p className="text-gray-600 dark:text-gray-300">{description}</p>
-      <p className="text-gray-800 dark:text-gray-100 mt-4 text-lg font-semibold">{price}</p>
-      <small className="text-gray-400 dark:text-gray-400">{students} students enrolled</small>
+      <p className="text-gray-800 dark:text-gray-100 mt-4 text-lg font-semibold">
+        {price}
+      </p>
+      <small className="text-gray-400 dark:text-gray-400">
+        {students} students enrolled
+      </small>
     </div>
   );
 };
 
-export default CourseCard
+export default CourseCard;
--- components/Courses.tsx
+++ components/Courses.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
-import Services from './Services';
-import CourseCard from './CourseCard';
+import React from "react";
+import Services from "./Services";
+import CourseCard from "./CourseCard";
+import imagePython from "../app/assets/python.jpg";
+import imageUrl from "../app/assets/ai.png";
+import imageGraphic from "../app/assets/graphics.jpg";
+import imageBootcamp from "../app/assets/bootcamp.png";
+import imageDataScience from "../app/assets/Python-for-data-analysis.jpg";
+import imageMobile from "../app/assets/apps.jpg";
 
 const Courses = () => {
   return (
@@ -9,23 +15,26 @@ const Courses = () => {
       <section>
         <h2 className="text-2xl font-bold mb-6">Featured Courses</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-          <CourseCard 
-            title="Complete Masterclass in Python" 
-            price="Tzs 350,000" 
-            students={150} 
+          <CourseCard
+            title="Complete Masterclass in Python"
+            price="Tzs 350,000"
+            students={150}
             description="Learn Python from scratch and become a master."
+            image={imagePython.src}
           />
-          <CourseCard 
-            title="AI (Artificial Intelligence)" 
-            price="Tzs 650,000" 
-            students={80} 
+          <CourseCard
+            title="AI (Artificial Intelligence)"
+            price="Tzs 650,000"
+            students={80}
             description="Dive into the world of AI and machine learning."
+            image={imageUrl.src}
           />
-          <CourseCard 
-            title="Graphics Design Masterclass" 
-            price="Tzs 650,000" 
-            students={100} 
+          <CourseCard
+            title="Graphics Design Masterclass"
+            price="Tzs 650,000"
+            students={100}
             description="Master design tools like Photoshop, Illustrator, and InDesign."
+            image={imageGraphic.src}
           />
         </div>
       </section>
@@ -34,28 +43,32 @@ const Courses = () => {
       <section>
         <h2 className="text-2xl font-bold mb-6">Upcoming Courses</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-          <CourseCard 
-            title="Web Development Bootcamp" 
-            price="Tzs 400,000" 
-            students={50} 
+          <CourseCard
+            title="Web Development Bootcamp"
+            price="Tzs 400,000"
+            students={50}
             description="A comprehensive guide to becoming a full-stack web developer."
+            image={imageBootcamp.src}
           />
-          <CourseCard 
-            title="Data Science with Python" 
-            price="Tzs 750,000" 
-            students={70} 
+          <CourseCard
+            title="Data Science with Python"
+            price="Tzs 750,000"
+            students={70}
             description="Analyze data and build powerful models with Python."
+            image={imageDataScience.src}
           />
-          <CourseCard 
-            title="Mobile App Development" 
-            price="Tzs 500,000" 
-            students={120} 
+
+          <CourseCard
+            title="Mobile App Development"
+            price="Tzs 500,000"
+            students={120}
             description="Learn to build mobile apps for Android and iOS platforms."
+            image={imageMobile.src}
           />
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Courses
+export default Courses;
