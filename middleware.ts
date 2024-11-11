5c6a985526422199cebaee636d1dbd1539969ee6
tacheraSasi <tacherasasi@gmail.com>
2024-11-11 20:12:32 +0300

    formatted the middleware.ts file

--- middleware.ts
+++ middleware.ts
@@ -1,122 +1,98 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-import { cookies } from 'next/headers';
-import { getUser } from './app/actions/user';
+import { cookies } from "next/headers";
+import { getUser } from "./app/actions/user";
 
-
-function matchURL(url:string, matchURL:string){
-    const regex = new RegExp(matchURL)
-    return regex.test(url)
+function matchURL(url: string, matchURL: string) {
+  const regex = new RegExp(matchURL);
+  return regex.test(url);
 }
 
-
 export async function middleware(req: NextRequest) {
-
-  const res = NextResponse.next()
-
+  const res = NextResponse.next();
 
   const token = await getUser();
- 
 
-  if(token.success === false){
-    if(matchURL(req.nextUrl.pathname,"/home(.*)")){
-        const redirectUrl = req.nextUrl.clone()
-        redirectUrl.pathname = '/'
-  return NextResponse.redirect(redirectUrl)
+  if (token.success === false) {
+    if (matchURL(req.nextUrl.pathname, "/home(.*)")) {
+      const redirectUrl = req.nextUrl.clone();
+      redirectUrl.pathname = "/";
+      return NextResponse.redirect(redirectUrl);
     }
 
-    if(matchURL(req.nextUrl.pathname,"/my-projects(.*)")){
-      const redirectUrl = req.nextUrl.clone()
-      redirectUrl.pathname = '/'
-return NextResponse.redirect(redirectUrl)
+    if (matchURL(req.nextUrl.pathname, "/my-projects(.*)")) {
+      const redirectUrl = req.nextUrl.clone();
+      redirectUrl.pathname = "/";
+      return NextResponse.redirect(redirectUrl);
+    }
+
+    if (matchURL(req.nextUrl.pathname, "/projects(.*)")) {
+      return NextResponse.next();
+    }
+
+    if (matchURL(req.nextUrl.pathname, "/courses(.*)")) {
+      return NextResponse.next();
+    }
+
+    if (matchURL(req.nextUrl.pathname, "/auth/login(.*)")) {
+      return NextResponse.next();
+    }
+
+    if (matchURL(req.nextUrl.pathname, "/auth/register(.*)")) {
+      return NextResponse.next();
+    }
+    if (matchURL(req.nextUrl.pathname, "/(.*)")) {
+      return NextResponse.next();
+    }
   }
 
-    if(matchURL(req.nextUrl.pathname,"/projects(.*)")){
-        
-  return NextResponse.next()
+  if (token.success === true) {
+    if (matchURL(req.nextUrl.pathname, "/home(.*)")) {
+      const redirectUrl = req.nextUrl.clone();
+      redirectUrl.pathname = "/";
+      return NextResponse.redirect(redirectUrl);
     }
 
-    if(matchURL(req.nextUrl.pathname,"/courses(.*)")){
-        
-        return NextResponse.next()
-          }
+    if (matchURL(req.nextUrl.pathname, "/projects(.*)")) {
+      return NextResponse.next();
+    }
 
-          if(matchURL(req.nextUrl.pathname,"/auth/login(.*)")){
-        
-            return NextResponse.next()
-              }
+    if (matchURL(req.nextUrl.pathname, "/my-projects(.*)")) {
+      return NextResponse.next();
+    }
 
-              if(matchURL(req.nextUrl.pathname,"/auth/register(.*)")){
-        
-                return NextResponse.next()
-                  }
-                  if(matchURL(req.nextUrl.pathname,"/(.*)")){
-        
-                    return NextResponse.next()
-                      }
+    if (matchURL(req.nextUrl.pathname, "/courses(.*)")) {
+      return NextResponse.next();
+    }
+
+    if (matchURL(req.nextUrl.pathname, "/auth/login(.*)")) {
+      const redirectUrl = req.nextUrl.clone();
+      redirectUrl.pathname = "/";
+      return NextResponse.redirect(redirectUrl);
+    }
+
+    if (matchURL(req.nextUrl.pathname, "/auth/register(.*)")) {
+      const redirectUrl = req.nextUrl.clone();
+      redirectUrl.pathname = "/";
+      return NextResponse.redirect(redirectUrl);
+    }
+
+    if (matchURL(req.nextUrl.pathname, "/(.*)")) {
+      return NextResponse.next();
+    }
   }
 
-
-  if(token.success === true){
-    if(matchURL(req.nextUrl.pathname,"/home(.*)")){
-        const redirectUrl = req.nextUrl.clone()
-        redirectUrl.pathname = '/'
-  return NextResponse.redirect(redirectUrl)
-    }
-
-    if(matchURL(req.nextUrl.pathname,"/projects(.*)")){
-        
-  return NextResponse.next()
-    }
-
-    if(matchURL(req.nextUrl.pathname,"/my-projects(.*)")){
-        
-      return NextResponse.next()
-        }
-
-    if(matchURL(req.nextUrl.pathname,"/courses(.*)")){
-        
-        return NextResponse.next()
-          }
-
-          if(matchURL(req.nextUrl.pathname,"/auth/login(.*)")){
-        
-            const redirectUrl = req.nextUrl.clone()
-        redirectUrl.pathname = '/'
-  return NextResponse.redirect(redirectUrl)
-              }
-
-              if(matchURL(req.nextUrl.pathname,"/auth/register(.*)")){
-        
-                const redirectUrl = req.nextUrl.clone()
-                redirectUrl.pathname = '/'
-          return NextResponse.redirect(redirectUrl)
-                  }
-
-                  if(matchURL(req.nextUrl.pathname,"/(.*)")){
-        
-                    return NextResponse.next()
-                      }
-
-  }
-   
-
- 
-
-  return res
+  return res;
 }
 
 export const config = {
   matcher: [
-    
-    '/',
-    '/auth/login',
-    '/auth/register',
-    '/courses',
-    '/projects',
-    '/my-projects'
-   
-    
+    "/",
+    "/auth/login",
+    "/auth/register",
+    "/courses",
+    "/projects",
+    "/my-projects",
   ],
-}
+};
